b017393d58879ddfb0d9112bb81a4d860603ceaf
huymax100720@gmail.com <huymax100720@gmail.com>
2023-03-16 14:12:38 +0700

    update

--- MongoModels/EventCard.cs
+++ MongoModels/EventCard.cs
@@ -10,7 +10,7 @@ namespace MobileBasedCashFlowAPI.MongoModels
         public string? id { get; set; }
         public string Event_name { get; set; } = string.Empty;
         public string Image_url { get; set; } = string.Empty;
-        public string Account_Name { get; set; } = string.Empty;
+        public string Account_name { get; set; } = string.Empty;
         public double Cost { get; set; } = double.MaxValue;
         public double Down_pay { get; set; } = double.MaxValue;
         public double Dept { get; set; } = double.MaxValue;
